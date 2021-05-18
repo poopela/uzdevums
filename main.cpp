09493a4f18f01f0dfc8c6e6e19533581f9ad8d6a
maksim mihailov <>
2021-05-18 05:02:03 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+int mas[100];
+cout << "ievadi skitļus" <<endl;
+cin >> n;
+cout << "uzraksti skaitļus" << endl;
+for (int i=0; i<n; i++)
+{
+   cin >> mas[i];
+}
+for (int i=0; i<n; i++)
+{ 
+cout << mas[1];
+}
+return 0;
+}
